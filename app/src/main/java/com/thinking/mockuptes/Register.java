04f43d0a8135701854a94fc5f1c14f454723d39a
bagusindrak <indra22071999@gmail.com>
2019-10-25 09:42:58 +0700

    Pertama

---
+++ app/src/main/java/com/thinking/mockuptes/Register.java
@@ -0,0 +1,18 @@
+package com.thinking.mockuptes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class Register extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+
+    public void onBtnRegisterNow_Click(View view) {
+    }
+}
